c523a7959f5727abdaa2eecf75eb66ea8c7ba270
ryanzam <ryanzam2005@yahoo.com>
2019-03-07 19:31:34 +0200

    Initial app

---
+++ build/precache-manifest.5f551c0c998ae3b77e28787691614175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "526f9e91f1d916485d68",
+    "url": "/static/js/main.526f9e91.chunk.js"
+  },
+  {
+    "revision": "0db56ea42d2aa5794b2d",
+    "url": "/static/js/2.0db56ea4.chunk.js"
+  },
+  {
+    "revision": "526f9e91f1d916485d68",
+    "url": "/static/css/main.ac2ceda9.chunk.css"
+  },
+  {
+    "revision": "4d0d02b3c1ebc5da954dacb97c291f75",
+    "url": "/index.html"
+  }
+];
